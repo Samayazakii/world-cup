75fc6b3ad138d44658aefa5ff9cdfb2c458a48d1
X <miohan@MacBook-Pro.local>
2018-03-14 15:15:27 +0800

    update:功能更新 选择功能写好 未添加样式

--- dist/js/index.js
+++ dist/js/index.js
@@ -62,7 +62,8 @@
     }
 
     // 每个小组三选一，与后面的二选一规则不太一样
-    global.getOneList = function () {
+    // 第一轮小组赛选择完执行的函数
+    function getOneList() {
         // 选中 01 小组赛晋级队伍
         var oneFormArr = document.querySelectorAll("#list-one form");
         // 记录获胜的队伍
@@ -91,21 +92,97 @@
         console.log('全部选择完全');
         // 第一轮选择结束记录组 id 到顶层变量
         roundOneArr = winTeamArr;
-    };
+
+        // 渲染 16 进 8 的数据
+        generateData(roundOneArr, document.getElementById("list-two"));
+    }
+
+    // 后面几轮选出胜者
+    function filterList(container) {
+        var formArr = container.getElementsByTagName('form');
+        var winTeamArr = [];
+
+        [].forEach.call(formArr, function (item, index) {
+            var inputArr = item.getElementsByTagName('input');
+            var selectRaioId = '';
+
+            [].forEach.call(inputArr, function (_it) {
+                if (_it.checked) {
+                    selectRaioId = _it.value;
+                }
+            });
+
+            if (!selectRaioId) {
+                // 提示选择完全
+                console.log('没有选择完全');
+
+                return;
+            } else {
+                winTeamArr.push(selectRaioId);
+            }
+        });
+
+        console.log('全部选择完全');
+
+        return winTeamArr;
+    }
 
     // 从剩 16 支队伍开始，使用生成函数，根据队生成选项，并渲染
     // 后面几轮的函数可以复用
     function generateData(teams, element) {
         var index = 0;
+        var teamObjArr = [];
+        var tplArr = [];
+        var tplStr = '';
 
-        // 在队伍池中挑选出该轮所有队伍信息
+        // 按照对阵的顺序存的，每次取两个生成一个对局
+        teamObjArr = teams.map(function (item, index) {
+            var obj = {};
+            var flag = false;
 
-        while (teams.length > 0) {
-            // 从中选两个队成一次比赛
+            for (var key in teamPoolObj) {
+                if (key == item) {
+                    flag = true;
+                    obj = teamPoolObj[key];
+                }
+            }
+
+            if (flag) {
+                return obj;
+            } else {
+                console.log('没有选完全');
+            }
+        });
+
+        while (teamObjArr.length > 0) {
+            // 选出相邻的两个队
+            var teamLeft = teamObjArr.shift();
+            var teamRight = teamObjArr.shift();
+            var prefix = teamLeft.val[0] + teamRight.val[0];
+
             // 渲染
+            tplArr.push('\n                <form id="' + prefix + '" class="two-form">\n                    <input type="radio" name="' + prefix + '" id="' + prefix + '-0" value="' + teamLeft.val + '">\n                    <label for="' + prefix + '-0" class="two-label">\n                        <p>' + teamLeft.val + '</p>\n                    </label>\n                \n                    <input type="radio" name="' + prefix + '" id="' + prefix + '-1" value="' + teamRight.val + '">\n                    <label for="' + prefix + '-1" class="two-label">\n                        <p>' + teamRight.val + '</p>\n                    </label>\n                </form>\n            ');
         }
+
+        tplStr = tplArr.join('\n');
+        element.innerHTML = tplStr;
     }
 
     // 执行首次生成数据
     initData();
+
+    // 导出到全局
+    global.getOneList = getOneList;
+    global.eightFour = function () {
+        roundTwoArr = filterList(document.getElementById("list-two"));
+        generateData(roundTwoArr, document.getElementById("list-thr"));
+    };
+    global.fourTwo = function () {
+        roundThrArr = filterList(document.getElementById("list-thr"));
+        generateData(roundThrArr, document.getElementById("list-fur"));
+    };
+    global.twoOne = function () {
+        roundFurArr = filterList(document.getElementById("list-fur"));
+        generateData(roundFurArr, document.getElementById("list-final"));
+    };
 })(window);
--- src/js/index.js
+++ src/js/index.js
@@ -88,7 +88,8 @@
     }
 
     // 每个小组三选一，与后面的二选一规则不太一样
-    global.getOneList = function () {
+    // 第一轮小组赛选择完执行的函数
+    function getOneList() {
         // 选中 01 小组赛晋级队伍
         let oneFormArr = document.querySelectorAll("#list-one form");
         // 记录获胜的队伍
@@ -117,22 +118,109 @@
         console.log('全部选择完全');
         // 第一轮选择结束记录组 id 到顶层变量
         roundOneArr = winTeamArr;
+
+        // 渲染 16 进 8 的数据
+        generateData(roundOneArr, document.getElementById("list-two"));
+    }
+
+    // 后面几轮选出胜者
+    function filterList(container) {
+        let formArr = container.getElementsByTagName('form');
+        let winTeamArr = [];
+
+        [].forEach.call(formArr, (item, index) => {
+            let inputArr = item.getElementsByTagName('input');
+            let selectRaioId = '';
+
+            [].forEach.call(inputArr, (_it) => {
+                if (_it.checked) {
+                    selectRaioId = _it.value;
+                }
+            });
+            
+            if (!selectRaioId) {
+                // 提示选择完全
+                console.log('没有选择完全');
+                
+                return;
+            } else {
+                winTeamArr.push(selectRaioId);
+            }
+        });
+
+        console.log('全部选择完全');
+
+        return winTeamArr;
     }
 
     // 从剩 16 支队伍开始，使用生成函数，根据队生成选项，并渲染
     // 后面几轮的函数可以复用
     function generateData(teams, element) {
         let index = 0;
-        
-        // 在队伍池中挑选出该轮所有队伍信息
+        let teamObjArr = [];
+        let tplArr = [];
+        let tplStr = '';
 
-        while (teams.length > 0) {
-            // 从中选两个队成一次比赛
+        // 按照对阵的顺序存的，每次取两个生成一个对局
+        teamObjArr = teams.map((item, index) => {
+            let obj = {};
+            let flag = false;
+            
+            for (let key in teamPoolObj) {
+                if (key == item) {
+                    flag = true;
+                    obj = teamPoolObj[key];
+                }
+            }
+
+            if (flag) {
+                return obj;
+            } else {
+                console.log('没有选完全');
+            }
+        });
+
+        while (teamObjArr.length > 0) {
+            // 选出相邻的两个队
+            let teamLeft = teamObjArr.shift();
+            let teamRight = teamObjArr.shift();
+            let prefix = teamLeft.val[0] + teamRight.val[0]
+
             // 渲染
+            tplArr.push(`
+                <form id="${prefix}" class="two-form">
+                    <input type="radio" name="${prefix}" id="${prefix}-0" value="${teamLeft.val}">
+                    <label for="${prefix}-0" class="two-label">
+                        <p>${teamLeft.val}</p>
+                    </label>
+                
+                    <input type="radio" name="${prefix}" id="${prefix}-1" value="${teamRight.val}">
+                    <label for="${prefix}-1" class="two-label">
+                        <p>${teamRight.val}</p>
+                    </label>
+                </form>
+            `);
         }
+
+        tplStr = tplArr.join('\n');
+        element.innerHTML = tplStr;
     }
 
     // 执行首次生成数据
     initData();
 
+    // 导出到全局
+    global.getOneList = getOneList;
+    global.eightFour = function () {
+        roundTwoArr = filterList(document.getElementById("list-two"));
+        generateData(roundTwoArr, document.getElementById("list-thr"));
+    }
+    global.fourTwo = function () {
+        roundThrArr = filterList(document.getElementById("list-thr"));
+        generateData(roundThrArr, document.getElementById("list-fur"));
+    }
+    global.twoOne = function () {
+        roundFurArr = filterList(document.getElementById("list-fur"));
+        generateData(roundFurArr, document.getElementById("list-final"));
+    }
 })(window);
